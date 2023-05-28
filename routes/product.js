8e06f3ed7ed10b539308ad50b51938417f35a463
Praveen <110990077+Praveenmuthu4@users.noreply.github.com>
2023-05-28 12:39:16 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -12,6 +12,7 @@ const router = express.Router();
 
 router.route("/product").get(getProduct);
 router.route("/product/:id").get(getOneProduct);
+router.route("/cart/product/:id").get(getOneProduct);
 
 router.route("/admin/product/newProduct").post(isAuthenticatedUser, newProduct);
 router
