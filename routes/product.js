6090b65bcfff2c824f5b715004c09cc6debe40a5
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-05-30 10:51:10 +0530

    added cart route

--- routes/product.js
+++ routes/product.js
@@ -12,11 +12,10 @@ const router = express.Router();
 
 router.route("/product").get(getProduct);
 router.route("/product/:id").get(getOneProduct);
-<<<<<<< HEAD
+
 router.route("/cart/:id").get(getOneProduct);
-=======
+
 router.route("/cart/product/:id").get(getOneProduct);
->>>>>>> 8e06f3ed7ed10b539308ad50b51938417f35a463
 
 router.route("/admin/product/newProduct").post(isAuthenticatedUser, newProduct);
 router
