b47ce08537b03af1394bd4b0a6ad992196ce1319
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-06-08 12:11:31 +0530

    changes done in app.js

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ const order = require("./routes/order");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middlewares/error");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const cloudinary = require("cloudinary");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+app.use(fileUpload());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use("/api", products);
 app.use("/api", user);
