2d1ae58096f8ae93e5b5569b0388632df2a9f3bc
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-06-08 13:05:46 +0530

    made changes in the server

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require("express");
 const server = express();
 const dotenv = require("dotenv");
 const app = require("./app");
+const cloudinary = require("cloudinary");
+
 require("./config/db");
 
 dotenv.config({ path: "backend/.env" });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 server.use("/", app);
 
