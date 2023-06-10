8eb5844dc8f6d03b232c9bf8b81f8bcad941f73b
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-06-10 19:59:07 +0530

    made changes in cors

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 app.use(fileUpload());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use("/api", products);
 app.use("/api", user);
