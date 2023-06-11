91e10c2bdfc9c799ef87984b48f20981304d3d71
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-06-11 12:34:35 +0530

    made some changes

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(isAuthenticatedUser, logout);
 router.route("/password/forgot").post(forgotPassword);
-router.route("/password/reset/:token").put(resetPassword);
+router.route("/password/reset/:token").put(isAuthenticatedUser, resetPassword);
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/me/update").put(isAuthenticatedUser, updateProfile);
